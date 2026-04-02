fccc39f22a878c47a15cd465d920684a48f8fcf1
NamrathaMedaragutta <nammi.xoxo24@gmail.com>
2026-04-02 20:46:57 +0530

    Day 4: Practiced for loop,while loop and even number program

---
+++ Day04/even_numbers.py
@@ -0,0 +1,5 @@
+# Day 4: Even Numbers using Loop
+
+for i in range(1, 11):
+    if i % 2 == 0:
+        print(i)
---
+++ Day04/for_loop.py
@@ -0,0 +1,6 @@
+# Day 4: For Loop Example
+
+print("Numbers from 1 to 5:")
+
+for i in range(1, 6):
+    print(i)
